1c5c06b45ce62475dc87e95e8a2554fcdf5cc89b
V.Piraisoodan <piraisoodanv1996@gmail.com>
2022-11-20 13:44:41 +0530

    initial commit

---
+++ json.js
@@ -0,0 +1,24 @@
+const data = {
+    name : "xxx",
+    email : "xxx@gmail.com",
+    phone : "123456789",
+    about : "aaa bbb ccc ddd",
+    experience : [{
+        company : "123",
+        experience : "2years",
+    },
+    {
+        company : "456",
+        experience : "1year"
+    }],
+    persnalInfo : {
+        name : "xxx",
+        fatherName : "yyy",
+        motherName : "zzz",
+        bloodgroup : "O",
+        dob : "xx-yy-xx",
+        address : "#5,aaa"
+    }
+}
+console.log(data)
+console.log(data.persnalInfo.dob)
